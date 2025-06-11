9333269517a07f12d464a5444a7f6870dcd5f010
marvingtx <marcusvinigt@gmail.com>
2025-06-10 22:44:01 -0300

    Module 3 Step 4 - Add metrics with Powertools

--- orders/.aws-sam/build/AddOrderFunction/create_order.py
+++ orders/.aws-sam/build/AddOrderFunction/create_order.py
@@ -4,13 +4,17 @@ from decimal import Decimal
 import json
 import uuid
 from datetime import datetime
-from aws_lambda_powertools import Logger
+
+from aws_lambda_powertools import Logger, Metrics
+from aws_lambda_powertools.metrics import MetricUnit
+
 from aws_lambda_powertools.utilities.typing import LambdaContext
 from aws_lambda_powertools.utilities.idempotency import (
     IdempotencyConfig, DynamoDBPersistenceLayer, idempotent_function
 )
 
 logger = Logger()
+metrics = Metrics()
 orders_table = os.getenv('TABLE_NAME')
 idempotency_table = os.getenv('IDEMPOTENCY_TABLE_NAME')
 dynamodb = boto3.resource('dynamodb')
@@ -49,12 +53,17 @@ def add_order(event: dict):
     # We must use conditional expression, otherwise put_item will always replace the original order and will never fail
     table.put_item(Item=ddb_item, ConditionExpression='attribute_not_exists(orderId) AND attribute_not_exists(userId)')
 
+    logger.info(f"new Order with ID {order_id} saved")
+    metrics.add_metric(name="SuccessfulOrder", unit=MetricUnit.Count, value=1)      #SuccessfulOrder
+    metrics.add_metric(name="OrderTotal", unit=MetricUnit.Count, value=total_amount) #OrderTotal
+    
     detail['orderId'] = order_id
     detail['orderTime'] = order_time
     detail['status'] = 'PLACED'
 
     return detail
 
+@metrics.log_metrics  # Ensure metrics are flushed after request completion or failure
 @logger.inject_lambda_context
 def lambda_handler(event, context: LambdaContext):
     idempotency_config.register_lambda_context(context)
